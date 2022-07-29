eeb75055a44d57f185f24a1559591b01e854aa46
Killer Boss Original <73131550+killerbossoriginal@users.noreply.github.com>
2022-07-29 16:22:30 +0200

    cpp

---
+++ c++/2 variabili.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+// Tipi di variabili: int, float, double, bool, char, string
+int main() {
+    /*
+    int a = 10;
+    float b = 3.3;
+    bool c = 0; // false
+    bool d = false;
+    bool e = 1; // true
+    bool f = true;
+    char g = 'g'
+    string h = 'Questa è una prova'
+    */
+}
---
+++ c++/3 input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    cout<<'Inserisci un numero '
+    cin>>numero;
+    cout<<'Hai inserito '
+    cout<<numero;
+
+    cout<<endl;
+
+    system('pause');
+}
